8d4c5abfcb24181364c0f697c0494440dd8cb323
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-15 18:19:59 +0200

    [add] header

--- resources/views/wines.blade.php
+++ resources/views/wines.blade.php
@@ -1,33 +1,35 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>Lista vini</h1>
+    <h1 class="text-center">Lista vini</h1>
+    <div class="container my-4 ">
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">winery</th>
-                <th scope="col">wine</th>
-                <th scope="col">rating_average</th>
-                <th scope="col">rating_reviews</th>
-                <th scope="col">location</th>
-                <th scope="col">image</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($wines as $wine)
+        <table class="table">
+            <thead>
                 <tr>
-
-                    <td>{{ $wine->id }}</td>
-                    <td>{{ $wine->winery }}</td>
-                    <td>{{ $wine->wine }}</td>
-                    <td>{{ $wine->rating_average }}</td>
-                    <td>{{ $wine->rating_reviews }}</td>
-                    <td>{{ $wine->location }}</td>
-                    <td>{{ $wine->image }}</td>
+                    <th scope="col">ID</th>
+                    <th scope="col">winery</th>
+                    <th scope="col">wine</th>
+                    <th scope="col">rating_average</th>
+                    <th scope="col">rating_reviews</th>
+                    <th scope="col">location</th>
+                    <th scope="col">image</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($wines as $wine)
+                    <tr>
+
+                        <td>{{ $wine->id }}</td>
+                        <td>{{ $wine->winery }}</td>
+                        <td>{{ $wine->wine }}</td>
+                        <td>{{ $wine->rating_average }}</td>
+                        <td>{{ $wine->rating_reviews }}</td>
+                        <td>{{ $wine->location }}</td>
+                        <td>{{ $wine->image }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
